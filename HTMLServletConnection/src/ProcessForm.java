163dabfe4c544bc8e05aaebefdb03a90ffb9257d
MeenuRaj <meenuraj979@gmail.com>
2015-08-21 16:26:57 -0400

    HTML to servelet to output.jsp

---
+++ HTMLServletConnection/src/ProcessForm.java
@@ -0,0 +1,50 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ProcessForm
+ */
+@WebServlet("/ProcessForm")
+public class ProcessForm extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProcessForm() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String f_name = request.getParameter("firstname");
+		String l_name = request.getParameter("lastname");
+		
+		String message = "My name is "+f_name+" "+l_name;
+		
+	      request.setAttribute("message", message);
+	      getServletContext()
+	      .getRequestDispatcher("/output.jsp")
+	      .forward(request,  response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
